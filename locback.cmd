31135b8ffa0f4d75e8b43128d0eb7f22e4a076e3
ug <zydeodict@gmail.com>
2017-02-06 23:17:51 +0100

    content: finished; DE xlations updated

--- locback.cmd
+++ locback.cmd
@@ -4,6 +4,7 @@ echo Copying content
 copy _locback\*.json		ZDO.CHSite\files\strings\
 copy _locback\*.js		ZDO.CHSite\wwwroot\dev-js\
 copy _locback\*.html		ZDO.CHSite\files\html\
+copy _locback\private\*.html	_private_content\
 
 echo Fixing array name in JS
 fart ZDO.CHSite\wwwroot\dev-js\strings.de.js uiStringsEn uiStringsDe
