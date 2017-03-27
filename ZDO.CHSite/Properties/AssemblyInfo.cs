a18479640833807e921f7c891a0f34069e19475a
ug <zydeodict@gmail.com>
2017-03-27 20:29:34 +0200

    version change

--- ZDO.CHSite/Properties/AssemblyInfo.cs
+++ ZDO.CHSite/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("ZDO.CHSite")]
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
 [assembly: ComVisible(false)]
