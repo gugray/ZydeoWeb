d4ef123fb527f856140007eea7b50384dafd001d
gugray <gabor@berlin.de>
2023-11-25 20:07:35 +0800

    dockerize WIP

--- publish.sh
+++ publish.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-[ -e chsite.tar.gz ] && rm chsite.tar.gz
 rm -rf ./_deploy_chsite
 dotnet publish ./ZDO.CHSite -c Release -o ./_deploy_chsite
-cd _deploy_chsite
-tar -czf ../chsite.tar.gz .
-cd ..
